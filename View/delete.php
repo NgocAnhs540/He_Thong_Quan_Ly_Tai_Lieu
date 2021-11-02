d414eb8b04199cb293f3748bf52d1c77bfbf58ce
unknown <tongthiquynh892001@gmail.com>
2021-11-02 23:17:53 +0700

    update xóa tài liệu

--- View/delete.php
+++ View/delete.php
@@ -1,5 +1,5 @@
 <?php
-include_once '../connection.php';
+include_once '../config/connection.php';
 //$sql1="SELECT Receipt FROM upload WHERE id=".$_REQUEST['data'];
 //
 //$res1=mysqli_query($con,$sql1) or die(mysqli_error($con));
@@ -9,7 +9,7 @@ $sql2="DELETE FROM upload WHERE id=".$_REQUEST['data'];
 $res2=mysqli_query($con,$sql2) or die(mysqli_error($con));
 if($res2)
 {
-    echo true;
+echo true;
 }
 mysqli_close($con);
 ?> 
