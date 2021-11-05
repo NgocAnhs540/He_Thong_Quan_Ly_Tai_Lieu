12347676f293cd335b12dc5e7ad94c00c4a7cb49
NgocAnhs540 <NA.ngocanh.31@gmail.com>
2021-11-05 20:29:05 +0700

    delete all

--- User/NormalSession.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    if($_SESSION['type']!='user')
-    {
-        header('location:../Login/login.php');
-        exit();
-    }
-    include '../config/connection.php'
-?>
